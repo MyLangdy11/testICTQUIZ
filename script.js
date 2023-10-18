4ab8877e27b74fbc2e095b5239e88da0568cc01c
MyLangdy11 <128284019+MyLangdy11@users.noreply.github.com>
2023-10-18 22:26:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
   const questions = [
     {
-      question: "1តើប្រទេសកម្ពុជាមានផ្ទៃដីប៉ុន្មានគីឡូម៉ែត្រ?",
-      image: "img/background.jpg",
-      options: ["១៨១,០៣៥ គីឡូម៉ែត្រការ៉េ", "១៩១,២៣៤ គីឡូម៉ែតការ៉េ", "៧៨៩,១២៣ គីឡូម៉ែតការ៉េ", "២៣៤,៩០០ គីឡូម៉ែតការ៉េ"],
-      answer: 0,
-      additionalInfo: "ប្រទេសកម្ពុជាមានផ្ទៃក្រឡា១៨១០៣៥ គីឡូម៉ែត្រការ៉េ មានព្រំប្រទល់ជាប់ថៃ ឡាវ វៀត​ណាម និងឈូងសមុទ្រ។មានព្រំប្រទល់ជាប់ថៃ ឡាវ វៀត​ណាម និងឈូងសមុទ្រ។មានព្រំប្រទល់ជាប់ថៃ ឡាវ វៀត​ណាម និងឈូងសមុទ្រ។មានព្រំប្រទល់ជាប់ថៃ ឡាវ វៀត​ណាម និងឈូងសមុទ្រ។មានព្រំប្រទល់ជាប់ថៃ ឡាវ វៀត​ណាម និងឈូងសមុទ្រ។មានព្រំប្រទល់ជាប់ថៃ ឡាវ វៀត​ណាម និងឈូងសមុទ្រ។"
-    },
+          question: "អ្វីទៅជាកុំព្យូទ័រ?",
+            image: "null",
+          options: ["កុំព្យូទ័រគឺជាម៉ាស៊ីនអេឡិចត្រូនិកដែលអាចបោះពុម្ពបាន និងអាចចាក់វីដេអូ រូបភាពជាដើម។","កុំព្យូទ័រគឺជាម៉ាស៊ីនដែលត្រូវការKeyboard Mouseដើម្បីដំណើរការ។","កុំព្យូទ័រគឺជាម៉ាស៊ីនអេឡិចត្រូនិក ដែលអាចបញ្ចូល គណនា រក្សាទុកទិន្នន័យ(ឯកសារ) និងបញ្ចេញព័ត៌មានមកក្រៅវិញ។","កុំព្យូទ័រគឺជាម៉ាស៊ីនដែលមានសារសំខាន់ជាងគេក្នុងជីវិតរស់នៅ។"],
+          answer: 2,
+                additionalInfo: "កុំព្យូទ័រគឺជាម៉ាស៊ីនអេឡិចត្រូនិក ដែលអាចបងញ្ចូល គណនា រក្សាទុកទិន្នន័យ(ឯកសារ) និងបញ្ចេញព័ត៌មានមកក្រៅវិញ។ វាជាឧបករណ៍អេឡិចត្រូនិកសម្រាប់ធ្វើការជាមួយព័ត៌មាន។ អ្នកអាចប្រើកុំព្យូទ័រសម្រាប់បង្កើតឯកសារ ដូចជា សរសេរសំបុត្រ គូរគំនូរ គណនា សរសេរកូដកម្មវិធី ឌីហ្សាញផ្សេងៗ។ល។"
+        },
+      {
+          question: "តើសមាសភាគសំខាន់ៗរបស់កុំព្យូទ័រមានប៉ុន្មាន?",
+            image: "null",
+          options: ["២","៣","៤","៦"],
+          answer: 2,
+               additionalInfo: "កុំព្យូទ័រផ្សំឡើងដោយសមាសភាគសំខាន់៤គឺ៖ ១.ធុងប្រព័ន្ធ(System Unit), ២.ម៉ូនីទ័រ(Monitor), ៣.ក្ដារចុច(Keyboard), ៤.កណ្ដុរ(Mouse)។"
+        },
+      {
+          question: "ក្នុងចំណោមចម្លើយខាងក្រោម មួយណាដែលជាមុខងាររបស់ធុងប្រព័ន្ធ (System Unit)?",
+            image: "null",
+          options: ["បង្ហាញព័ត៌មានផ្សេងៗ ដែលកំពុងដំណើរការក្នុងកុំព្យូទ័រ","ជាឧបករណ៍បញ្ចូលទិន្នន័យ","ជាធុងដែលផ្ទុកនូវសមាសភាគសំខាន់ៗសម្រាប់ឱ្យប្រព័ន្ធដំណើរការបាន","ជាឧបករណ៍អនុញ្ញាត្តឱ្យយើងបញ្ជាព្រួញកណ្ដុរបាន"],
+          answer: 2,
+                additionalInfo: "ធុងប្រព័ន្ធ គឺជាធុងមួយដែលផ្ទុកនូវសមាសភាគសំខាន់ៗសម្រាប់ឱ្យប្រព័ន្ធកុំព្យូទ័រដំណើរការបាន។ សមាសភាគទាំងនោះមានដូចជា បន្ទះមេ(Motherboard) ខួរក្បាល(CPU) ថាសរឹង(Hard Drive) សតិ(RAM) កាតបន្ថែម(Expansion Card) ដ្រាយស៊ីឌីរ៉ូម(CD-ROM Drive)។ល។ សមាសភាគនេះវាដូចជាខួរក្បាលរបស់មនុស្ស ដែលមានតួនាទីយ៉ាងសំខាន់ក្នុងការគិត និងធ្វើឱ្យប្រព័ន្ធទាំងមូលដំណើរការបាន។"
+        },
+      {
+          question: "ក្នុងចំណោមចម្លើយខាងក្រោម មួយណាដែលជាតួរនាទីរបស់កណ្ដុរ(Mouse)?",
+            image: "null",
+          options: ["បង្ហាញព័ត៌មានផ្សេងៗ ដែលកំពុងដំណើរការក្នុងកុំព្យូទ័រ","ជាឧបករណ៍បញ្ចូលទិន្នន័យ","ជាធុងដែលផ្ទុកនូវសមាសភាគសំខាន់ៗសម្រាប់ឱ្យប្រព័ន្ធដំណើរការបាន","ជាឧបករណ៍កាន់ដោយដៃ ដែលអនុញ្ញាតឱ្យយើងបញ្ជាព្រួញកណ្ដុរ ដែលស្ថិតលើអេក្រង់ ដើម្បីជ្រើសរើសរូបតំណាង ឬជ្រើសម៉ឺនុយណាមួយ។"],
+          answer: 3,
+                additionalInfo: "កណ្ដុរ(Mouse) ជាឧបករណ៍កាន់ដោយដៃ ដែលអនុញ្ញាតឱ្យយើងបញ្ជាព្រួញកណ្ដុរ ដែលស្ថិតលើអេក្រង់ ដើម្បីជ្រើសរើសរូបតំណាង ឬជ្រើសម៉ឺនុយណាមួយ។ វាត្រូវបានប្រើដោយភ្ជាប់ទៅនឹងធុងប្រព័ន្ធតាមរយៈច្រក(Port) PS/2 ឬUSB។"
+        },
+      {
+          question: "ក្នុងចំណោមចម្លើយខាងក្រោម ចម្លើយមួយណាដែលជាមុខងាររបស់ម៉ូនីទ័រ(Monitor)?",
+            image: "null",
+          options: ["ជាឧបករណ៍បញ្ចូលទិន្នន័យ","ជាឧបករណ៍បង្ហាញព័ត៌មានដែលកំពុងដំណើរការក្នុងកុំព្យូទ័រ រាងដូចទូរទស្សន៍","ជាឧបករណ៍បញ្ជារព្រួញកណ្ដុរ","ជាឧបការផ្ទុក ដូចនឹងខួរក្បាលមនុស្ស"],
+          answer: 1,
+          explanation: "ម៉ូនីទ័រ(Monitor) គឺជាឧបករណ៍មានរាងដូចទូរទស្សន៍ដែលមាននាទីសម្រាប់បង្ហាញព័ត៌មានផ្សេងៗ ដែលកំពុងដំណើរការលើកុំព្យូទ័រ។ វាត្រូវបានគេប្រើដោយភ្ជាប់ជាមួយនឹងធុងប្រព័ន្ធតាមរយៈច្រក(Port) VGA។"
+        },
+      
+    // {
+    //   question: "",
+    //   image: "null",
+    //   options: ["", "", "", ""],
+    //   answer: 0,
+    //   additionalInfo: ""
+    // },
     // Add more questions here...
   ];
 
@@ -25,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const timerElement = document.querySelector(".timer");
   const gameHistoryElement = document.querySelector(".game-history");
   const playAgainBtn = document.getElementById("play-again-btn");
+  const exitButton=document.getElementById("exitButton");
 
   // Sound effects
   const correctSound = new Audio("sound/correct.mp3");
@@ -128,7 +165,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
       const progressPercentage = (timeLeft / 60) * 100;
       progressBar.style.width = `${progressPercentage}%`;
-      if (timeLeft<=10){
+      if (timeLeft<=4){
         timeisout.play();
       }
       if (timeLeft === 0) {
@@ -148,9 +185,10 @@ document.addEventListener("DOMContentLoaded", function() {
   function showCorrectAnswer() {
     const correctOption = optionsElement.children[currentQuestion.answer];
     correctOption.classList.add("correct");
-    resultElement.textContent = "Time's up!";
+    resultElement.textContent = "អស់ពេលហើយ!";
     resultElement.classList.add("incorrect");
     showAdditionalInfo();
+    scoreIncorrect++;
   }
 
   function endGame() {
@@ -164,18 +202,23 @@ document.addEventListener("DOMContentLoaded", function() {
     const numOfQuestions = selectedQuestions.length;
     const numOfCorrectAnswers = document.querySelectorAll(".correct").length;
     const numOfIncorrectAnswers = numOfQuestions - numOfCorrectAnswers;
+    
+    const percentCorrect = ((scoreCorrect / numOfQuestions) * 100).toFixed(2);
+    const percentIncorrect = ((scoreIncorrect/ numOfQuestions) * 100).toFixed(2);
   
     document.getElementById("quiz-container").style.display = "none";
     document.querySelector(".game-history").style.display="block";
     gameHistoryElement.innerHTML = `
       <h2>Game History</h2>
-      <p>ចម្លើយត្រឹមត្រូវ: ${scoreCorrect}</p>
-      <p>ចម្លើយមិនត្រឹមត្រូវ: ${scoreIncorrect}</p>
       <p>ចំនួនសំណួរទាំងអស់: ${numOfQuestions}</p>
+      <p>ចម្លើយត្រឹមត្រូវ: ${scoreCorrect} ស្មើនឹង ${percentCorrect}%</p>
+      <p>ចម្លើយមិនត្រឹមត្រូវ: ${scoreIncorrect} ស្មើនឹង ${percentIncorrect}%</p>
     `;
   
     playAgainBtn.style.display = "block";
+    exitButton.style.display="block";
   }
+
 
   playAgainBtn.addEventListener("click", function() {
     startGame();
